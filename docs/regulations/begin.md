a121350d01475e5a19dbe03d9076ae68df344bfe
Vladimir Ochakovskiy <leo2208948@mail.ru>
2024-06-06 19:54:24 +0700

    Начальная структура

---
+++ docs/regulations/begin.md
@@ -0,0 +1,6 @@
+---
+sidebar_position: 1
+sidebar_label: Начало разработки
+title: Начало разработки
+description: Начало разработки
+---
--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/regulations">
             Начать погружение ~ 30мин ⏱️
           </Link>
         </div>
